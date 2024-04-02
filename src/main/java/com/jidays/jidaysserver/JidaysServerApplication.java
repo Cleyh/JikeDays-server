111d477cd6744026d994fae645c5a697def2e43d
Cleyh <unbd_233@qq.com>
2024-04-02 20:30:04 +0800

    创建项目

---
+++ src/main/java/com/jidays/jidaysserver/JidaysServerApplication.java
@@ -0,0 +1,13 @@
+package com.jidays.jidaysserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JidaysServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JidaysServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jidays/jidaysserver/JidaysServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jidays.jidaysserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JidaysServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
