bd549c7ba508c6d65655e5da315a1fbaf2d91759
Lso3214 <2492979242@qq.com>
2024-04-09 21:30:28 +0800

    这是解决cors任意端口问题的新类

---
+++ src/main/java/com/jidays/jidaysserver/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.jidays.jidaysserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 允许所有域访问，也可以指定特定域，例如："http://localhost:8081"
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
+                .allowedHeaders("*"); // 允许的请求头部
+    }
+}
